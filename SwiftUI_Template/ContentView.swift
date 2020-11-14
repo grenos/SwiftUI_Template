a57bb64fd35b60f28dcb538ff1b2f47fce7fb792
vasilis <vasilis.green@gmail.com>
2020-11-14 14:35:58 +0100

    Initial Commit

---
+++ SwiftUI_Template/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Template
+//
+//  Created by Vasileios  Gkreen on 14/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
