5507547aa737f5c4e759a7d0cf93f4ccbdbcce91
vasilis <vasilis.green@gmail.com>
2020-12-07 15:09:48 +0100

    adds networking with alamoFire for GET and GET with parameters

---
+++ SwiftUI_Template/Models/TodoModel.swift
@@ -0,0 +1,8 @@
+//
+//  TodoModel.swift
+//  SwiftUI_Template
+//
+//  Created by Vasileios  Gkreen on 07/12/20.
+//
+
+import Foundation
---
+++ SwiftUI_Template/NetworkManager/NetworkParameters.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkParameters.swift
+//  SwiftUI_Template
+//
+//  Created by Vasileios  Gkreen on 07/12/20.
+//
+
+import Foundation
